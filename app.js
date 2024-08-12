1d1273f5ecdff5e020bf6fa4bb4f6d78f9fb6678
Bareera rao <bareeramannan@gmail.com>
2024-08-12 19:14:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let x = 5, y = 10, z = 15;
+
+let firstName;
+let $age;
+let _city;
+let user1;
+let totalAmount;
+
+let 1stUser;      // Starts with a number
+let user-name;    // Contains a hyphen
+let var;          // Reserved keyword
+let total amount; // Contains a space
+let @price;       // Contains special character other than $ or _
